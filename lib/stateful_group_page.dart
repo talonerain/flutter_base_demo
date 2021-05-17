ebda89c27a1038acc5896d6d6a511fb428f051df
mingyan <mingyan@pinduoduo.com>
2021-05-17 11:23:29 +0800

    TextField输入框控件、PageView轮播控件

--- lib/stateful_group_page.dart
+++ lib/stateful_group_page.dart
@@ -107,6 +107,26 @@ class _StateFulGroupState extends State<StateFulGroup> {
                             'https://www.devio.org/img/avatar.png',
                             width: 100,
                             height: 100,
+                          ),
+                          TextField(
+                            //输入文本样式
+                            decoration: InputDecoration(
+                                contentPadding: EdgeInsets.fromLTRB(5, 0, 0, 0),
+                                hintText: '请输入',
+                                hintStyle: TextStyle(fontSize: 15)),
+                          ),
+                          Container(
+                            height: 100,
+                            margin: EdgeInsets.only(top: 50),
+                            decoration:
+                                BoxDecoration(color: Colors.lightBlueAccent),
+                            child: PageView(
+                              children: <Widget>[
+                                _item('Page1', Colors.deepPurple),
+                                _item('Page2', Colors.green),
+                                _item('Page2', Colors.red),
+                              ],
+                            ),
                           )
                         ],
                       ),
@@ -123,5 +143,16 @@ class _StateFulGroupState extends State<StateFulGroup> {
   Future<Null> _handleRefresh() async {
     await Future.delayed(Duration(milliseconds: 200));
     return null;
+  }
+
+  _item(String title, Color color) {
+    return Container(
+      alignment: Alignment.center,
+      decoration: BoxDecoration(color: color),
+      child: Text(
+        title,
+        style: TextStyle(fontSize: 22, color: Colors.white),
+      ),
+    );
   }
 }
